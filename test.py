7d13dced376e01729c6cc4a37df5600f220c90e9
Дмитрий Старостенок <49001181+Hlebnica@users.noreply.github.com>
2023-03-15 22:09:13 +0500

    lab2_end

--- test.py
+++ test.py
@@ -1,39 +1,133 @@
+import random
 import pandas as pd
-import numpy as np
+import sys
 
-S = ["read", "write", "grant"]  # Набор операций над объектами
-U = ["User1", "User2", "User3", "User4", "User5", "User6"]  # Множество субъектов
-O = ["Object1", "Object2", "Object3", "Object4", "Object5"]  # Множество объектов доступа
+S = ["Read", "Write", "Grant privileges", "Forbidden", "All privileges"] # Набор привелегий
+users = ("Admin", "User1", "User2", "User3", "User4", "User5") # Субъекты 
+user_count = len(users)
+objects = ("Object1", "Object2", "Object3", "Object4", "Object5") # Объекты
+object_count = len(objects)
+
+# Заполнения матрицы доступа
+
+access_matrix = {}
+
+for user in users:
+    access_matrix[user] = {}
+    for OBJECT in objects:
+        if user == "Admin":
+            access_matrix[user][OBJECT] = ["All privileges"]
+        else:
+            privileges_count = random.randint(1, 3)
+            privileges = []
+            match privileges_count:
+                case 1:
+                    privilege = random.randint(0, 4)
+                    privileges.append(S[privilege])
+                case _:
+                    tmp_priv = random.sample(S[0:3], privileges_count)
+                    for i in range(privileges_count):
+                        privileges.append(tmp_priv[i])
+
+            access_matrix[user][OBJECT] = privileges
 
 
-def get_random_rights(access_rights):
-    rights_ = np.random.choice(access_rights, size=np.random.choice(len(access_rights) + 1), replace=False)
+print(pd.DataFrame.from_dict(access_matrix).T)
 
-    if len(rights_) == 0 or len(rights_) == 1 and "grant" in rights_:
-        rights_ = ["ban"]
-    elif len(rights_) == len(access_rights):
-        rights_ = ["full access"]
-    return rights_
-
-
-access_matrix = pd.DataFrame(columns=[f"Object{n + 1}" for n in range(len(O))], index=U[:len(U)])
-access_matrix = access_matrix.applymap(lambda x: get_random_rights(S))
-
-access_matrix.iloc[[np.random.randint(len(access_matrix.index))]] = [[['full access']]]
-print(access_matrix)
+# Авторизация пользователя и вывод его прав
 
 while True:
-    user_id = input("Введите идентификатор пользователя: ")
+    print("login", end="")
+    cur_user = input()
 
-    if user_id not in U:
-        print("Неверный идентификатор пользователя.")
+    if cur_user not in users:
+        print("Wrong username!")
         continue
-    else:
-        user_rights = access_matrix.loc[user_id]
-        print(f"Идентификация прошла успешно, добро пожаловать в систему, {user_id}!")
-        print("Перечень Ваших прав:")
 
-        for obj, rights in user_rights.items():
-            print(f"{obj}: {', '.join(rights)}")
+    else:
+        print(f'User: {cur_user}')
+        print('Identification successful, welcome to the system!')
+        print('List of your privileges:')
+        for _ in access_matrix[cur_user]:
+            privileges= ""
+            for priv in access_matrix[cur_user][_]:
+                if len(privileges) == 0:
+                    privileges += priv
+                else:
+                    privileges += ", " + priv
+            print(f'\t{_}: {privileges}')
+    break
+
+# Обработка команд по матрице доступа
+
+while True:
+    command = input("Enter command > ")
+
+    if command == "quit":
+        print(f"The work of the user {cur_user} is finished. Goodbye.")
+        cur_user = None
         break
 
+    if command not in ("read", "write", "grant"):
+        print("Wrong command")
+        sys.stdout.flush()
+        continue
+
+    # Чтение файла
+    elif command == "read": 
+        print("Which object is being operated on? (read)")
+        cur_obj = input()
+        if cur_obj not in objects:
+            print("There is no such object in the system.")
+            sys.stdout.flush()
+            continue
+        if "Read" not in access_matrix[cur_user][cur_obj] and "All privileges" not in access_matrix[cur_user][cur_obj]:
+            print("Refusal to perform an operation. You have no rights to perform it.")
+            sys.stdout.flush()
+            continue
+        print("The operation was successful! (read)")
+
+    # Запись в файл
+    elif command == "write": 
+        print("Which object is being operated on? (write)")
+        cur_obj = input()
+        if cur_obj not in objects:
+            print("There is no such object in the system.")
+            sys.stdout.flush()
+            continue
+        if "Write" not in access_matrix[cur_user][cur_obj] and "All privileges" not in access_matrix[cur_user][cur_obj]:
+            print("Refusal to perform an operation. You have no rights to perform it.")
+            sys.stdout.flush()
+            continue
+        print("The operation was successful! (write)")
+
+    # Передача прав доступа
+    elif command == "grant": 
+        print("Which object is being operated on? (grant)")
+        cur_obj = input()
+        if cur_obj not in objects:
+            print("There is no such object in the system.")
+            sys.stdout.flush()
+            continue
+        if "Grant privileges" not in access_matrix[cur_user][cur_obj] and "All privileges" not in access_matrix[cur_user][cur_obj]:
+            print("Refusal to perform an operation. You have no rights to perform it.")
+            sys.stdout.flush()
+            continue
+        print("What privilege is being transferred? (Read/Write/Grant privileges/Forbidden/All privileges)")
+        cur_privilege = input()
+        if cur_privilege not in S:
+            print("There is no such privilege in the system. ")
+            sys.stdout.flush()
+            continue
+        print("To which user is the right transferred? ")
+
+        selected_user = input()
+        if selected_user not in users:
+            print("There is no such user in the system.")
+            sys.stdout.flush()
+            continue
+        if cur_privilege not in access_matrix[selected_user][cur_obj] and "All privileges" not in access_matrix[selected_user][cur_obj]:
+            access_matrix[selected_user][cur_obj].append(cur_privilege)
+            print("The operation was successful.")
+        else:
+            print("User already has this privilege.")
